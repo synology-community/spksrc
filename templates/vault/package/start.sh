0811baa2acfa3d9b9b3413621eab295841540750
appkins <nbatkins@gmail.com>
2024-07-17 05:48:46 -0500

    vault

--- templates/vault/package/start.sh
+++ templates/vault/package/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/var/packages/vault/target/bin/vault server -config "/var/packages/vault/shares/vault/etc/vault.d"
+/var/packages/vault/target/bin/vault server -dev -dev-root-token-id root -config "/var/packages/vault/shares/vault/etc/vault.d"
