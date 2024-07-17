8d309ebf8f97be38af8cc257c54512af80b181f7
appkins <nbatkins@gmail.com>
2024-07-17 13:25:34 -0500

    nomad root

--- templates/nomad/package/start.sh
+++ templates/nomad/package/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 /var/packages/nomad/target/bin/nomad agent -config "/var/packages/nomad/shares/nomad/etc/nomad.d/nomad.hcl" -config "/var/packages/nomad/shares/nomad/etc/nomad.d/"
