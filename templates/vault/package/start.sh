8eeaae91be5d6b555a54f8848cecc93b6fc04b7b
appkins <nbatkins@gmail.com>
2024-07-17 04:45:19 -0500

    configure vault paths

--- templates/vault/package/start.sh
+++ templates/vault/package/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/var/packages/vault/target/bin/vault server -config "/var/packages/vault/shares/vault/etc/vault.d/vault.hcl"
+/var/packages/vault/target/bin/vault server -config "/var/packages/vault/shares/vault/etc/vault.d"
